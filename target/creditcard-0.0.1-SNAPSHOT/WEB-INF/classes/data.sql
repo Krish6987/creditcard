299dc9cf1b6e17d07e0536cda2ff41b37e4fd49e
ga20088046 <ga20088046@wipro.com>
2020-09-21 09:49:41 +0530

    first commit

--- target/creditcard-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO CreditScore values ('APPXA2244X',9.50);
-INSERT INTO CreditScore values ('AXVPS7766V',1.50);
-INSERT INTO CreditScore values ('ASXPS2121S',5.10);
-INSERT INTO CreditScore values ('AXSSP1122H',3.10);
-INSERT INTO CreditScore values ('APPSA3355P',8.50);
